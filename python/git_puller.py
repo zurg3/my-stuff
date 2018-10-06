b137dbce567278494096d712cc4cd828f239c3d3
zurg3 <20664125+zurg3@users.noreply.github.com>
2018-10-06 20:38:16 +0300

    Edited git_puller.py

--- python/git_puller.py
+++ python/git_puller.py
@@ -11,15 +11,28 @@ print('Example:', example_path)
 git_path = str(input('-> '))
 git_repos = os.listdir(git_path)
 print()
-
-for i in range(len(git_repos)):
-    git_pull_path = str(git_path + '/' + git_repos[i])
-    os.chdir(git_pull_path)
-    percentage_of_completion = (i + 1) / len(git_repos) * 100
-    percentage_of_completion = round(percentage_of_completion, 1)
-    percentage_of_completion = str(percentage_of_completion)
-    print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
-    print(git_repos[i])
-    os.system('git pull')
-    print(percentage_of_completion + '% done.')
-    print()
+git_path_len = len(git_path) - 1
+if git_path[git_path_len] != '/':
+    for i in range(len(git_repos)):
+        git_pull_path = str(git_path + '/' + git_repos[i])
+        os.chdir(git_pull_path)
+        percentage_of_completion = (i + 1) / len(git_repos) * 100
+        percentage_of_completion = round(percentage_of_completion, 1)
+        percentage_of_completion = str(percentage_of_completion)
+        print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
+        print(git_repos[i])
+        os.system('git pull')
+        print(percentage_of_completion + '% done.')
+        print()
+elif git_path[git_path_len] == '/':
+    for i in range(len(git_repos)):
+        git_pull_path = str(git_path + git_repos[i])
+        os.chdir(git_pull_path)
+        percentage_of_completion = (i + 1) / len(git_repos) * 100
+        percentage_of_completion = round(percentage_of_completion, 1)
+        percentage_of_completion = str(percentage_of_completion)
+        print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
+        print(git_repos[i])
+        os.system('git pull')
+        print(percentage_of_completion + '% done.')
+        print()
