f41d1b7b26ae6a9cd6e6b4227a9f8cdcd68f1713
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-05-15 13:31:32 +0300

    Added imdb_list_ids.js

---
+++ java-script/imdb_list_ids.js
@@ -0,0 +1,8 @@
+var list_items = document.getElementsByClassName('lister-item-header');
+var list_items_num = list_items.length;
+for (var i = 0; i < list_items_num; i++) {
+  var list_item_title = list_items[i].innerText;
+  var list_item = list_items[i].children[1].children[0].attributes[0].nodeValue;
+  var list_item_id = list_item.split('/');
+  console.log(list_item_title + ' - ' + list_item_id[2]);
+}
