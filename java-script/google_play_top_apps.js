4ea79b8d5f57af54f58a4af34472ec5248558a44
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-07-06 14:28:47 +0300

    Edited google_play_top_apps.js

--- java-script/google_play_top_apps.js
+++ java-script/google_play_top_apps.js
@@ -1,4 +1,6 @@
 var top_len = prompt('Enter list length');
+var top_num = 1;
 for (var i = 0; i < top_len; i++) {
-  console.log(document.getElementsByClassName('details')[i].innerText);
+  console.log(top_num + ". " + document.getElementsByClassName('WsMG1c nnK0zc')[i].innerText);
+  var top_num = top_num + 1;
 }
