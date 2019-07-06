75ef79a574f72a22ef2f859f11819044f2b490f0
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-07-06 14:33:13 +0300

    Edited imdb_list_ids.js

--- java-script/imdb_list_ids.js
+++ java-script/imdb_list_ids.js
@@ -1,3 +1,5 @@
+// You need open a list in compact view to use this script
+
 var list_items = document.getElementsByClassName('lister-item-header');
 var list_items_num = list_items.length;
 for (var i = 0; i < list_items_num; i++) {
