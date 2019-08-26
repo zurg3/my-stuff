db03a2a4f57cd5658acc079752f7981979c0ed26
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-08-26 23:04:22 +0300

    Added imdb_names_list_sort.js

---
+++ java-script/imdb_names_list_sort.js
@@ -0,0 +1,8 @@
+var names_array = [];
+for (var i = 0; i < document.getElementsByClassName('lister-item-header').length; i++) {
+  names_array.push(document.getElementsByClassName('lister-item-header')[i].children[1].innerText);
+}
+names_array.sort();
+for (var j = 0; j < names_array.length; j++) {
+  console.log(names_array[j]);
+}
