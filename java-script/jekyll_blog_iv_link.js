8629084a7e16f61171087f1d702d006eabcf1d33
zurg3 <20664125+zurg3@users.noreply.github.com>
2019-11-27 18:05:08 +0300

    Edited some files

--- java-script/jekyll_blog_iv_link.js
+++ java-script/jekyll_blog_iv_link.js
@@ -1,3 +1,3 @@
 var post_url = window.location.href;
-var iv_rhash = "e779dfb8ed6d71";
-console.log('https://t.me/iv?url=' + post_url + '&rhash=' + iv_rhash);
+var iv_rhash = 'e779dfb8ed6d71';
+console.log(`https://t.me/iv?url=${post_url}&rhash=${iv_rhash}`);
