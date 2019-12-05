e98c3e2b0362d9f91b5bc2a48b5861a867451aea
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2019-12-05 17:02:33 +0300

    Edited imdb_contributions_count.js

--- java-script/imdb_contributions_count.js
+++ java-script/imdb_contributions_count.js
@@ -5,8 +5,12 @@ var submitted_contributions_num = contributions_table.children.length - 1;
 var contribution_items_num = 0;
 
 for (var i = 1; i < submitted_contributions_num; i++) {
-  var contribution_item = contributions_table.children[i].children[3].innerText.split(' item')[0];
-  contribution_items_num = contribution_items_num + parseInt(contribution_item[contribution_item.length - 1], 10);
+  var contribution_item_summary = contributions_table.children[i].children[3].innerText;
+  var contribution_item_summary_lines = contribution_item_summary.split('\n').length;
+  for (var j = 1; j <= contribution_item_summary_lines - 2; j++) {
+    var contribution_item = contribution_item_summary.split('\n')[j].split(' item')[0];
+    contribution_items_num = contribution_items_num + parseInt(contribution_item[contribution_item.length - 1], 10);
+  }
 }
 
 console.log('Total IMDb contributions: ' + contribution_items_num);
