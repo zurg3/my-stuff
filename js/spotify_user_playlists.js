1f444b308f3da0cb67a9b8bc7c395c919654a45c
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-07-22 14:54:13 +0300

    Edited spotify_user_playlists.js

--- js/spotify_user_playlists.js
+++ js/spotify_user_playlists.js
@@ -5,7 +5,9 @@ for (var i = 0; i < playlists; i++) {
   playlist.push(document.getElementsByClassName('contentSpacing')[1].children[1].children[i].children[0].children[0].children[1].children[0].children[0].innerText);
 }
 
-playlist.sort();
+playlist.sort(function(a, b) {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+});
 
 for (var j = 0; j < playlists; j++) {
   console.log(playlist[j]);
