3d2f6326c35ce56ce1f53962886ad4d4ed9bc3a1
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-07-30 19:14:46 +0300

    Edited git_puller.py

--- python/git_puller.py
+++ python/git_puller.py
@@ -1,5 +1,4 @@
 # this program probably works only on Linux.
-# I recommend you add this script to shell's aliases (file .bashrc)
 
 import sys
 import os
@@ -13,31 +12,28 @@ else:
     print('Enter the path of directory with cloned Git repositories.')
     # print('Example:', example_path)
     git_path = str(input('-> '))
+
 git_repos = os.listdir(git_path)
+
 git_repos.sort(key=str.lower)
+
 print()
+
 git_path_len = len(git_path) - 1
-if git_path[git_path_len] != '/':
-    for i in range(len(git_repos)):
+
+for i in range(len(git_repos)):
+    if git_path[git_path_len] != '/':
         git_pull_path = str(git_path + '/' + git_repos[i])
-        os.chdir(git_pull_path)
-        percentage_of_completion = (i + 1) / len(git_repos) * 100
-        percentage_of_completion = round(percentage_of_completion, 1)
-        percentage_of_completion = str(percentage_of_completion)
-        print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
-        print(git_repos[i])
-        os.system('git pull')
-        print(percentage_of_completion + '% done.')
-        print()
-elif git_path[git_path_len] == '/':
-    for i in range(len(git_repos)):
+    elif git_path[git_path_len] == '/':
         git_pull_path = str(git_path + git_repos[i])
-        os.chdir(git_pull_path)
-        percentage_of_completion = (i + 1) / len(git_repos) * 100
-        percentage_of_completion = round(percentage_of_completion, 1)
-        percentage_of_completion = str(percentage_of_completion)
-        print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
-        print(git_repos[i])
-        os.system('git pull')
-        print(percentage_of_completion + '% done.')
-        print()
+
+    os.chdir(git_pull_path)
+
+    percentage_of_completion = (i + 1) / len(git_repos) * 100
+    percentage_of_completion = str(round(percentage_of_completion, 1))
+
+    print(str(str(i + 1)) + ' / ' + str(len(git_repos)))
+    print(git_repos[i])
+    os.system('git pull')
+    print(percentage_of_completion + '% done.')
+    print()
