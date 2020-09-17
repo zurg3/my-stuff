c798dd2b40d6c8aba69cbcaa91ee1eb0c40ebef8
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-09-17 18:22:34 +0300

    Removed google_play_top_apps.js

--- js/google_play_top_apps.js
+++
@@ -1,6 +0,0 @@
-var top_len = prompt('Enter list length');
-var top_num = 1;
-for (var i = 0; i < top_len; i++) {
-  console.log(top_num + ". " + document.getElementsByClassName('WsMG1c nnK0zc')[i].innerText);
-  var top_num = top_num + 1;
-}
