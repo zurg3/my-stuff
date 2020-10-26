6987222e093703d44d5ca67502aea1142736ac8e
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-10-26 13:31:12 +0300

    Edited calc.py

--- python/calc.py
+++ python/calc.py
@@ -1,7 +1,7 @@
 a = float(input('Enter the first number: '))
 b = float(input('Enter the second number: '))
 
-print("%g" % a + '+' + "%g" % b + '=' + "%g" % (a + b))
-print("%g" % a + '-' + "%g" % b + '=' + "%g" % (a - b))
-print("%g" % a + '*' + "%g" % b + '=' + "%g" % (a * b))
-print("%g" % a + '/' + "%g" % b + '=' + "%g" % (a / b))
+print('%g+%g=%g' % (a, b, (a + b)))
+print('%g-%g=%g' % (a, b, (a - b)))
+print('%g*%g=%g' % (a, b, (a * b)))
+print('%g/%g=%g' % (a, b, (a / b)))
