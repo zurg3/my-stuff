83e03bb1b114b39afced50902c5f7a50024e5651
zurg3 <20664125+zurg3@users.noreply.github.com>
2021-01-18 16:51:34 +0300

    Edited RandomInteger.java

--- java/RandomInteger.java
+++ java/RandomInteger.java
@@ -1,6 +1,7 @@
 // returns a random integer from 1 to 10
 public class RandomInteger {
   public static void main(String[] args) {
-    System.out.println(Math.round(Math.floor(Math.random() * 10) + 1));
+    //System.out.println(Math.round(Math.floor(Math.random() * 10) + 1));
+    System.out.println(Math.round(Math.floor(Math.random() * (10 - 1 + 1)) + 1));
   }
 }
