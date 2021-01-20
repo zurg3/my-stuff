ad16b03ad773732f2d9f613fc6c8de995e4c040e
zurg3 <20664125+zurg3@users.noreply.github.com>
2021-01-20 17:32:33 +0300

    Edited some files

--- cpp/unix_time.cpp
+++ cpp/unix_time.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
 #include <ctime>
 #include <iomanip>
@@ -9,62 +10,54 @@
 #endif
 using namespace std;
 
+void print_separator() {
+  cout << "|--------------------------------------------------------|" << endl;
+}
+
+void print_table_head() {
+  print_separator();
+  cout << "| UTC Time            | Local Time          | UNIX Time  |" << endl;
+  print_separator();
+}
+
+void print_time() {
+  time_t now = time(NULL);
+
+  tm local_time = *localtime(&now);
+  tm utc_time = *gmtime(&now);
+  int unix_time = now;
+
+  cout << "| ";
+  cout << put_time(&utc_time, "%H:%M:%S %d.%m.%Y");
+  cout << " | ";
+  cout << put_time(&local_time, "%H:%M:%S %d.%m.%Y");
+  cout << " | ";
+  cout << unix_time;
+  cout << " |" << endl;
+
+  #if defined(WIN32) || defined(_WIN32)
+    Sleep(1000);
+  #else
+    sleep(1);
+  #endif
+}
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    cout << "|--------------------------------------------------------|" << endl;
-    cout << "| UTC Time            | Local Time          | UNIX Time  |" << endl;
-    cout << "|--------------------------------------------------------|" << endl;
+    print_table_head();
 
     while (true) {
-      time_t now = time(NULL);
-
-      tm local_time = *localtime(&now);
-      tm utc_time = *gmtime(&now);
-      int unix_time = now;
-
-      cout << "| ";
-      cout << put_time(&utc_time, "%H:%M:%S %d.%m.%Y");
-      cout << " | ";
-      cout << put_time(&local_time, "%H:%M:%S %d.%m.%Y");
-      cout << " | ";
-      cout << unix_time;
-      cout << " |" << endl;
-
-      #if defined(WIN32) || defined(_WIN32)
-        Sleep(1000);
-      #else
-        sleep(1);
-      #endif
+      print_time();
     }
   }
   else if (argc == 2) {
-    cout << "|--------------------------------------------------------|" << endl;
-    cout << "| UTC Time            | Local Time          | UNIX Time  |" << endl;
-    cout << "|--------------------------------------------------------|" << endl;
+    print_table_head();
 
     for (int i = 0; i < stoi(argv[1]); i++) {
-      time_t now = time(NULL);
-
-      tm local_time = *localtime(&now);
-      tm utc_time = *gmtime(&now);
-      int unix_time = now;
-
-      cout << "| ";
-      cout << put_time(&utc_time, "%H:%M:%S %d.%m.%Y");
-      cout << " | ";
-      cout << put_time(&local_time, "%H:%M:%S %d.%m.%Y");
-      cout << " | ";
-      cout << unix_time;
-      cout << " |" << endl;
-
-      #if defined(WIN32) || defined(_WIN32)
-        Sleep(1000);
-      #else
-        sleep(1);
-      #endif
+      print_time();
     }
 
-    cout << "|--------------------------------------------------------|" << endl;
+    print_separator();
   }
   else {
     cout << "Something is wrong!" << endl;
--- python/unix_time.py
+++ python/unix_time.py
@@ -8,38 +8,39 @@ def unix__time():
     return u
 
 
+def print_separator():
+    print('|--------------------------------------------------------|')
+
+
+def print_table_head():
+    print_separator()
+    print('| UTC Time            | Local Time          | UNIX Time  |')
+    print_separator()
+
+
+def print_time():
+    now_time_local = datetime.datetime.now()
+    local_time = now_time_local.strftime('%H:%M:%S %d.%m.%Y')
+    now_time_utc = datetime.datetime.utcnow()
+    utc_time = now_time_utc.strftime('%H:%M:%S %d.%m.%Y')
+    unix_time = unix__time()
+    local_time = str(local_time)
+    utc_time = str(utc_time)
+    unix_time = str(unix_time)
+    print('| ' + utc_time + ' | ' + local_time + ' | ' + unix_time + ' |')
+    time.sleep(1)
+
+
 k = input('k = ')
 if k == '':
-    print('|--------------------------------------------------------|')
-    print('| UTC Time            | Local Time          | UNIX Time  |')
-    print('|--------------------------------------------------------|')
+    print_table_head()
     while True:
-        now_time_local = datetime.datetime.now()
-        local_time = now_time_local.strftime('%H:%M:%S %d.%m.%Y')
-        now_time_utc = datetime.datetime.utcnow()
-        utc_time = now_time_utc.strftime('%H:%M:%S %d.%m.%Y')
-        unix_time = unix__time()
-        local_time = str(local_time)
-        utc_time = str(utc_time)
-        unix_time = str(unix_time)
-        print('| ' + utc_time + ' | ' + local_time + ' | ' + unix_time + ' |')
-        time.sleep(1)
+        print_time()
 elif k != '':
     k = int(k)
-    print('|--------------------------------------------------------|')
-    print('| UTC Time            | Local Time          | UNIX Time  |')
-    print('|--------------------------------------------------------|')
+    print_table_head()
     for i in range(k):
-        now_time_local = datetime.datetime.now()
-        local_time = now_time_local.strftime('%H:%M:%S %d.%m.%Y')
-        now_time_utc = datetime.datetime.utcnow()
-        utc_time = now_time_utc.strftime('%H:%M:%S %d.%m.%Y')
-        unix_time = unix__time()
-        local_time = str(local_time)
-        utc_time = str(utc_time)
-        unix_time = str(unix_time)
-        print('| ' + utc_time + ' | ' + local_time + ' | ' + unix_time + ' |')
-        time.sleep(1)
-    print('|--------------------------------------------------------|')
+        print_time()
+    print_separator()
 else:
     print('Something is wrong!')
