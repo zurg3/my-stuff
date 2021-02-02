c421850a80edc2742a0f12103b3b3bb8dc4d6e75
zurg3 <20664125+zurg3@users.noreply.github.com>
2021-02-02 19:09:31 +0300

    Added Go files

---
+++ go/calc.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a float32
+  var b float32
+
+  fmt.Print("Enter the first number: ")
+  fmt.Scanf("%f", &a)
+  fmt.Print("Enter the second number: ")
+  fmt.Scanf("%f", &b)
+
+  fmt.Printf("%g+%g=%g\n", a, b, a + b)
+  fmt.Printf("%g-%g=%g\n", a, b, a - b)
+  fmt.Printf("%g*%g=%g\n", a, b, a * b)
+  fmt.Printf("%g/%g=%g\n", a, b, a / b)
+}
