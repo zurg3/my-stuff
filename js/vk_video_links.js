65b31fae159c164c5d451f7b823cc7484b64fa47
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2025-02-16 19:05:21 +0300

    Removed some files

--- js/vk_video_links.js
+++
@@ -1,8 +0,0 @@
-var video_links = [];
-for (var i = 0; i < document.getElementsByClassName('lnk').length; i++) {
-  video_links[i] = document.getElementsByClassName('lnk')[i].href;
-  video_links[i] = video_links[i].split('?')[0];
-}
-for (var j = 0; j < video_links.length; j++) {
-  console.log(video_links[j]);
-}
