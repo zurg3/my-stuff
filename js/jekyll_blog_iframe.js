d9643674a148aa2ecb47c921ddfbe07b04dfbf41
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-03-18 16:33:09 +0300

    Edited jekyll_blog_iframe.js

--- js/jekyll_blog_iframe.js
+++ js/jekyll_blog_iframe.js
@@ -5,11 +5,9 @@ if (article && iframes.length > 0) {
   const article_width = article.offsetWidth;
 
   for (let i = 0; i < iframes.length; i++) {
-    const iframe_width = parseInt(iframes[i].width, 10);
-
-    if (iframes[i].width !== '100%' && article_width < iframe_width) {
+    if (iframes[i].width !== '100%' && article_width < parseInt(iframes[i].width, 10)) {
       iframes[i].width = article_width;
-      iframes[i].height = Math.floor(iframe_width / 1.77);
+      iframes[i].height = Math.floor(article_width / 1.77);
     }
   }
 }
