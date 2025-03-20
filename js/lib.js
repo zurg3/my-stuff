1708883f79117fb58e5e7b90f8d7cd808ed5e54c
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-03-20 19:33:22 +0300

    Edited some files

--- js/lib.js
+++ js/lib.js
@@ -11,6 +11,26 @@ function random_number(min = 1, max = 100) {
   }
 }
 
+function sum(...numbers) {
+  if (numbers.length > 0) return numbers.reduce((total, num) => total + num, 0);
+}
+
+function multiply(...numbers) {
+  if (numbers.length > 0) return numbers.reduce((total, num) => total * num, 1);
+}
+
+function average(...numbers) {
+  if (numbers.length > 0) return sum(...numbers) / numbers.length;
+}
+
+function digital_root(n) {
+  if (typeof n === 'number') return (n - 1) % 9 + 1;
+}
+
+function reverse_string(s) {
+  if (typeof s === 'string') return s.split('').reverse().join('');
+}
+
 function comparator(a, b) {
   return a.toLowerCase().localeCompare(b.toLowerCase());
 }
