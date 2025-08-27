3841569b449419bf437d6136de83c5f2ea349169
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-08-27 17:55:13 +0300

    Added Rust files

---
+++ rust/calc.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut a = String::new();
+    let mut b = String::new();
+
+    print!("Enter the first number: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut a).unwrap();
+    let a:f32 = a.trim().parse().unwrap();
+
+    print!("Enter the second number: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut b).unwrap();
+    let b:f32 = b.trim().parse().unwrap();
+
+    println!("{}+{}={}", a, b, a + b);
+    println!("{}-{}={}", a, b, a - b);
+    println!("{}*{}={}", a, b, a * b);
+    println!("{}/{}={}", a, b, a / b);
+}
