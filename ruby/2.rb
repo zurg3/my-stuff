226fcd82a90950e74e734aa66afc3a1d342da3fd
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-08-28 15:47:20 +0300

    Renamed some files

--- ruby/2.rb
+++ ruby/2.rb
@@ -1,3 +1,10 @@
-(0..10).each do |i|
-  puts "Ruby #{i}"
-end
+print "Your first name: "
+first_name = gets.chomp
+print "Your last name: "
+last_name = gets.chomp
+print "Your country: "
+country = gets.chomp
+
+puts "I'm #{first_name} #{last_name} from #{country}" # output via puts
+print "I'm ", first_name, " ", last_name, " from ", country # output via print
+puts # empty line
