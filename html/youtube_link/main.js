f132a3ec07da73262fa1780f384eb26577baca10
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-12-01 17:50:21 +0300

    Edited some files

--- html/youtube_link/main.js
+++ html/youtube_link/main.js
@@ -15,6 +15,9 @@ if (is_mobile()) {
 const input_block = document.getElementById('input');
 const original_link = document.getElementById('original_link');
 
+const video_option = document.getElementById('video_option');
+const playlist_option = document.getElementById('playlist_option');
+
 const video_links_block = document.getElementById('video_links');
 const yt_video_link = document.getElementById('yt_video_link');
 const ytm_video_link = document.getElementById('ytm_video_link');
@@ -46,42 +49,53 @@ function convert() {
     const original_url = new URL(original_link.value);
     const params = Object.fromEntries(original_url.searchParams.entries());
 
+    let valid_url;
+
     if (['www.youtube.com', 'm.youtube.com', 'music.youtube.com', 'piped.video'].includes(original_url.host) && (params.v || params.list)) {
-      video_id = params.v;
-      playlist_id = params.list;
+      valid_url = true;
+      video_id = video_option.checked ? params.v : '';
+      playlist_id = playlist_option.checked ? params.list : '';
     }
     else if (original_url.host === 'youtu.be' && original_url.search) {
+      valid_url = true;
       video_id = original_url.pathname.split('/')[1];
     }
     else if (['www.youtube.com', 'm.youtube.com', 'youtube.com'].includes(original_url.host) && ['/shorts/', '/live/', '/embed/'].some(path => original_url.pathname.search(path) === 0)) {
+      valid_url = true;
       video_id = original_url.pathname.split('/')[2];
     }
     else {
+      valid_url = false;
       alert('Invalid URL');
     }
 
-    if (video_id) {
-      const yt_url = `https://youtu.be/${video_id}`;
-      const ytm_url = `https://music.youtube.com/watch?v=${video_id}`;
-      const ppd_url = `https://piped.video/watch?v=${video_id}`;
+    if (valid_url) {
+      clear_output();
+      remove_video();
 
-      video_links_block.hidden = false;
+      if (video_id) {
+        const yt_url = `https://youtu.be/${video_id}`;
+        const ytm_url = `https://music.youtube.com/watch?v=${video_id}`;
+        const ppd_url = `https://piped.video/watch?v=${video_id}`;
 
-      yt_video_link.innerHTML = `<a href="${yt_url}" target="_blank">${yt_url}</a>`;
-      ytm_video_link.innerHTML = `<a href="${ytm_url}" target="_blank">${ytm_url}</a>`;
-      ppd_video_link.innerHTML = `<a href="${ppd_url}" target="_blank">${ppd_url}</a>`;
-    }
+        video_links_block.hidden = false;
 
-    if (playlist_id) {
-      const yt_url = `https://www.youtube.com/playlist?list=${playlist_id}`;
-      const ytm_url = `https://music.youtube.com/playlist?list=${playlist_id}`;
-      const ppd_url = `https://piped.video/playlist?list=${playlist_id}`;
+        yt_video_link.innerHTML = `<a href="${yt_url}" target="_blank">${yt_url}</a>`;
+        ytm_video_link.innerHTML = `<a href="${ytm_url}" target="_blank">${ytm_url}</a>`;
+        ppd_video_link.innerHTML = `<a href="${ppd_url}" target="_blank">${ppd_url}</a>`;
+      }
 
-      playlist_links_block.hidden = false;
+      if (playlist_id) {
+        const yt_url = `https://www.youtube.com/playlist?list=${playlist_id}`;
+        const ytm_url = `https://music.youtube.com/playlist?list=${playlist_id}`;
+        const ppd_url = `https://piped.video/playlist?list=${playlist_id}`;
 
-      yt_playlist_link.innerHTML = `<a href="${yt_url}" target="_blank">${yt_url}</a>`;
-      ytm_playlist_link.innerHTML = `<a href="${ytm_url}" target="_blank">${ytm_url}</a>`;
-      ppd_playlist_link.innerHTML = `<a href="${ppd_url}" target="_blank">${ppd_url}</a>`;
+        playlist_links_block.hidden = false;
+
+        yt_playlist_link.innerHTML = `<a href="${yt_url}" target="_blank">${yt_url}</a>`;
+        ytm_playlist_link.innerHTML = `<a href="${ytm_url}" target="_blank">${ytm_url}</a>`;
+        ppd_playlist_link.innerHTML = `<a href="${ppd_url}" target="_blank">${ppd_url}</a>`;
+      }
     }
   }
 }
@@ -102,9 +116,7 @@ function play_video() {
   }
 }
 
-function clear_input() {
-  original_link.value = '';
-
+function clear_output() {
   yt_video_link.innerHTML = '';
   ytm_video_link.innerHTML = '';
   ppd_video_link.innerHTML = '';
@@ -115,10 +127,17 @@ function clear_input() {
 
   video_links_block.hidden = true;
   playlist_links_block.hidden = true;
+}
 
+function remove_video() {
   yt_iframe.src = '';
   yt_iframe.remove();
+}
 
+function clear_input() {
+  original_link.value = '';
+  clear_output();
+  remove_video();
   video_id = '';
   playlist_id = '';
 }
