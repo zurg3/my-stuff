6b862e6d835091b22f9a9a6bf9ed47749ddc571b
zurg3 <20664125+zurg3@users.noreply.github.com>
2026-03-25 17:40:21 +0300

    Edited lib.js

--- js/lib.js
+++ js/lib.js
@@ -2,16 +2,14 @@
 
 const lib = {};
 
-function is_mobile() {
-  return window.screen.width < window.screen.height;
-}
+lib.is_mobile = () => window.screen.width < window.screen.height;
 
-function back_to_top() {
+lib.back_to_top = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+};
 
-function is_valid_url(s) {
+lib.is_valid_url = (s) => {
   if (typeof s !== 'string') return false;
 
   try {
@@ -21,70 +19,70 @@ function is_valid_url(s) {
   catch (error) {
     return false;
   }
-}
+};
 
-function random_number(min = 1, max = 100) {
+lib.random_number = (min = 1, max = 100) => {
   if ((typeof min === 'number' && typeof max === 'number') && (min <= max)) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   else {
     return 0;
   }
-}
+};
 
-function sum(...numbers) {
+lib.random_id = () => crypto.randomUUID();
+
+lib.sum = (...numbers) => {
   if (numbers.length > 0) return numbers.reduce((total, num) => total + num, 0);
-}
+};
 
-function multiply(...numbers) {
+lib.multiply = (...numbers) => {
   if (numbers.length > 0) return numbers.reduce((total, num) => total * num, 1);
-}
+};
 
-function average(...numbers) {
-  if (numbers.length > 0) return sum(...numbers) / numbers.length;
-}
+lib.average = (...numbers) => {
+  if (numbers.length > 0) return lib.sum(...numbers) / numbers.length;
+};
 
-function digital_root(n) {
+lib.digital_root = (n) => {
   if (typeof n === 'number') return (n - 1) % 9 + 1;
-}
+};
 
-function reverse_string(s) {
+lib.reverse_string = (s) => {
   if (typeof s === 'string') return s.split('').reverse().join('');
-}
+};
 
-function title_case(s, extended = false) {
+lib.upper_case = (s) => {
+  if (typeof s === 'string') return s.toUpperCase();
+};
+
+lib.lower_case = (s) => {
+  if (typeof s === 'string') return s.toLowerCase();
+};
+
+lib.title_case = (s, extended = false) => {
   if (typeof s === 'string') {
     const reg_exp = extended ? /([- ])/ : /([ ])/;
 
     return s.toLowerCase().split(reg_exp).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
   }
-}
+};
 
-function swap_case(s) {
+lib.swap_case = (s) => {
   if (typeof s === 'string') return s.split('').map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join('');
-}
+};
 
-function comparator(a, b) {
-  return a.toLowerCase().localeCompare(b.toLowerCase());
-}
+lib.comparator = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
 
-function unique_array(array) {
-  return [...new Set(array)];
-}
+lib.unique_array = (array) => [...new Set(array)];
 
-function count_of(array, value) {
-  return array.filter(item => item === value).length;
-}
+lib.count_of = (array, value) => array.filter(item => item === value).length;
 
-function is_any_radio_checked(radio_name) {
-  return document.querySelector(`input[name="${radio_name}"]:checked`) !== null;
-}
+lib.is_any_radio_checked = (radio_name) => document.querySelector(`input[name="${radio_name}"]:checked`) !== null;
 
-function get_checked_radio_value(radio_name) {
-  return document.querySelector(`input[name="${radio_name}"]:checked`).value;
-}
+lib.get_checked_radio_value = (radio_name) => document.querySelector(`input[name="${radio_name}"]:checked`).value;
 
-function parse_data_legacy(url, type) {
+lib.parse_data_legacy = (url, type) => {
   const data_types = ['html', 'json', 'text'];
 
   if (!url || !data_types.includes(type)) return false;
@@ -96,9 +94,15 @@ function parse_data_legacy(url, type) {
     xhr.send();
 
     if (xhr.status >= 200 && xhr.status < 300) {
-      if (type === 'html') return new DOMParser().parseFromString(xhr.responseText, 'text/html');
-      if (type === 'json') return JSON.parse(xhr.responseText);
-      if (type === 'text') return xhr.responseText;
+      if (type === 'html') {
+        return new DOMParser().parseFromString(xhr.responseText, 'text/html');
+      }
+      else if (type === 'json') {
+        return JSON.parse(xhr.responseText);
+      }
+      else if (type === 'text') {
+        return xhr.responseText;
+      }
     }
     else {
       return false;
@@ -107,9 +111,9 @@ function parse_data_legacy(url, type) {
   catch (error) {
     return false;
   }
-}
+};
 
-async function parse_data(url, type) {
+lib.parse_data = async (url, type) => {
   const data_types = ['html', 'json', 'text'];
 
   if (!url || !data_types.includes(type)) return false;
@@ -119,15 +123,19 @@ async function parse_data(url, type) {
 
     if (!res.ok) return false;
 
-    if (type === 'html') return new DOMParser().parseFromString(await res.text(), 'text/html');
-    if (type === 'json') return await res.json();
-    if (type === 'text') return await res.text();
+    if (type === 'html') {
+      return new DOMParser().parseFromString(await res.text(), 'text/html');
+    }
+    else if (type === 'json') {
+      return await res.json();
+    }
+    else if (type === 'text') {
+      return await res.text();
+    }
   }
   catch (error) {
     return false;
   }
-}
+};
 
-function sleep(t = 1) {
-  return new Promise(r => setTimeout(r, t * 1000));
-}
+lib.sleep = (t = 1) => new Promise(r => setTimeout(r, t * 1000));
